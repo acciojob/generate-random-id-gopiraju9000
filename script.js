01735b7da89184f82e57402bef0a1e8f6550b397
gopiraju9000 <144523885+gopiraju9000@users.noreply.github.com>
2025-03-31 01:01:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function makeid(l) {
   // write your code here
+	return Math.random().toString(36).substring(2, 2 + l);
 }
 
-// Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
